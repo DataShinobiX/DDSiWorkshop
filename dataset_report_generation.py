41b36a436fca262004370ffd667d8b68aea81626
Paritosh Singh <paritoshsingh@Paritoshs-MacBook-Air.local>
2025-03-21 21:10:01 +0100

    Initial commmit| Includes dataset report generated

---
+++ dataset_report_generation.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from ydata_profiling import ProfileReport
+
+transactions = pd.read_csv('Comfy_Data/Transactions.csv', sep = ';') 
+customers = pd.read_csv('Comfy_Data/Client_information.csv', sep = ';')
+
+# Display basic information about the datasets
+print("Transactions Dataset Information:")
+transactions.info()
+print("\n\nCustomers Dataset Information:")
+customers.info()
+
+# Show summary statistics
+print("\nTransactions Dataset Summary Statistics:")
+transactions.describe()
+print("\n\nCustomers Dataset Summary Statistics:")
+customers.describe()
+
+# Generate pandas-profiling report for transactions dataset
+profile_transactions = ProfileReport(transactions, title="Transactions Data Quality Report")
+profile_transactions.to_file("Reports/transactions_data_quality_report.html")
+
+# Generate pandas-profiling report for customers dataset
+profile_customers = ProfileReport(customers, title="Customers Data Quality Report")
+profile_customers.to_file("Reports/customers_data_quality_report.html")
